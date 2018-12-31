1c586207226566485d70a5819df1240961c5ba96
Joachim Schulze <joschulze1@t-online.de>
2018-12-31 12:52:31 +0100

    multiply Strings

--- src/main/kotlin/org/joachimalaya/operators/Modifications.kt
+++ src/main/kotlin/org/joachimalaya/operators/Modifications.kt
@@ -15,3 +15,19 @@ operator fun <K> List<K>.times(times: Int) = multiplyByAddingWithNeutral({ a, b 
  * This function will throw an [IllegalArgumentException] if given a multiplicand less than 0.
  */
 operator fun <K> Int.times(toMultiply: List<K>) = multiplyByAddingWithNeutral({ a, b -> a + b }, toMultiply, this, emptyList())
+
+/**
+ * 'Multiply' a String by concatenating it to itself repeatedly.
+ * If a String is multiplied with 0 the result is the empty String.
+ *
+ * This function will throw an [IllegalArgumentException] if given a multiplicand less than 0.
+ */
+operator fun String.times(times: Int) = multiplyByAddingWithNeutral(String::plus, this, times, "")
+
+/**
+ * 'Multiply' a String by concatenating it to itself repeatedly.
+ * If a String is multiplied with 0 the result is the empty String.
+ *
+ * This function will throw an [IllegalArgumentException] if given a multiplicand less than 0.
+ */
+operator fun Int.times(toMultiply: String) = multiplyByAddingWithNeutral(String::plus, toMultiply, this, "")
